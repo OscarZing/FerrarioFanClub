ec7193ae20f9719f37e868b972328d9d5c7daa35
Oscar Röhr <oscar@ancus.de>
2025-12-05 14:46:03 +0100

    Moneten in slots intergriert

--- mainFile/Slots.py
+++ mainFile/Slots.py
@@ -1,34 +1,55 @@
 def slots():
     import random
+    from Bank import Kontostand
     
     print('Wilkommen zum Slot Machine Spiel!')
+    print('Jeder Spielzug kostet 5 moneten.')
+    spielWahl = input('Willst du spielen? (j/n): ')
+    if spielWahl == 'j':   
 
-    symbols = ['🍒', '🍋', '🍊', '🍉', '⭐', '🔔', '7️⃣ ']
+        symbols = ['🍒', '🍋', '🍊', '🍉', '⭐', '🔔', '7️⃣ ']
 
-    while True:
-        slot_1 = random.randint(0,6)
-        slot_2 = random.randint(0,6)
-        slot_3 = random.randint(0,6)
+        while True:
+            print('Dein aktueller Kontostand ist:', Kontostand)
+            Kontostand -=5
+            if Kontostand < 5:           
+                print("Du hast nicht genug moneten, um zu spielen!")
+                break
+            else:
+                slot_1 = random.randint(0,6)
+                slot_2 = random.randint(0,6)
+                slot_3 = random.randint(0,6)
 
-        print(slot_1, slot_2, slot_3)
+#                print(slot_1, slot_2, slot_3)
 
-        print(symbols[slot_1], symbols[slot_2], symbols[slot_3])
+                print(symbols[slot_1], symbols[slot_2], symbols[slot_3])
 
-        if slot_1 == slot_2 == slot_3:
-            if slot_1 == 6:
-                print("Mega Jackpot!")
-            print("Jackpot!")
+                if slot_1 == slot_2 == slot_3:
+                    if slot_1 == 6:
+                        Kontostand += 1005
+                        print("Mega Jackpot! Du hast 1000 moneten gewonnen!")
+                        print("Dein neuer Kontostand ist:", Kontostand)
 
-        else:
-            print("Nächstes mal gewinnst du!")
+                    else:
+                        Kontostand += 205   
+                        print("Jackpot! Du hast 200 moneten gewonnen!")
+                        print("Dein neuer Kontostand ist:", Kontostand)
 
-        stopInput = input("Möchtest du nochmal spielen? (j/n): ")
-        if stopInput == 'j':
-            continue
-        elif stopInput == 'n':
-            break
-        else:
-            print("Ungültige Eingabe, Spiel wird beendet.")
-            break
+                else:
+                    print("Nächstes mal gewinnst du!")
+                    print("Dein neuer Kontostand ist:", Kontostand)
+
+                stopInput = input("Möchtest du nochmal spielen? (j/n): ")
+                if stopInput == 'j':
+                    continue
+                elif stopInput == 'n':
+                    break
+                else:
+                    print("Ungültige Eingabe, Spiel wird beendet.")
+                    break
+    elif spielWahl == 'n':
+        print("Spiel wird beendet.")
+    else:
+        print("Ungültige Eingabe, Spiel wird beendet.")
 
 slots()
