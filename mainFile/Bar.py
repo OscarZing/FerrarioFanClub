cec8c2279fdadc034059317f86ee180bf90e0e3f
Oscar Röhr <oscar@ancus.de>
2025-12-25 01:00:13 +0100

    ade bar agfange

--- mainFile/Bar.py
+++ mainFile/Bar.py
@@ -1,4 +1,43 @@
-def bar():
-    import random
-    from Bank import Kontostand
-    print('Wilkommen zur Bar!')
+import dbtest
+
+def menu():
+    print("Hier ist die Getränkekarte:")
+    print("-----------------------------------")
+    print("Nummer | Drinkname     | Preis | Alkoholgehalt(%) | Menge(cl)")
+    print("-----------------------------------")
+
+    drinks = dbtest.get_all_drinks()
+    for drink in drinks:
+        print(f"{drink[0]:<6} | {drink[1]:<13} | {drink[2]:<5} | {drink[3]:<16} | {drink[4]:<8}")
+
+
+
+def bar(username, taschengeld):
+
+
+    print('Willkommen in der Bar!')
+    wahl = input('Möchtest du die Getränkekarte sehen? (j/n): ')
+
+
+    if wahl == 'j':
+        
+        while wahl == 'j':
+            menu()
+            drinkwahl=input('welchen drink möchten sie bestellen? geben die die Nummer ein: ')
+            if int(drinkwahl) not in [num[0] for num in dbtest.get_all_numbers()]:
+                print("Ungültige Eingabe. Bitte geben Sie eine gültige Nummer ein.")
+                continue
+            else:
+                print("Du hast", dbtest.get_drink_name(drinkwahl), "bestellt.")
+                taschengeld -= dbtest.get_drink_price(drinkwahl)
+                print("Du hast jetzt:", taschengeld, "moneten in der Tasche.")
+
+
+            
+
+
+
+
+if __name__ == "__main__":
+   
+    bar()
--- mainFile/dbtest.py
+++ mainFile/dbtest.py
@@ -8,10 +8,16 @@ def create_table(tablename):
     else:
         match tablename:
             case 'user':
-                sql = "CREATE TABLE user(username PRIMARY KEY, password)"
+                sql = "CREATE TABLE user(username PRIMARY KEY, password, betrunkenheit DEFAULT 0)"
                 
             case 'bank':
                 sql = "CREATE TABLE bank(user PRIMARY KEY, kontostand, schulden DEFAULT 0)"
+            
+            case "casino":
+                sql = "CREATE TABLE casino(user PRIMARY KEY, )"
+            
+            case "drinks":
+                sql = "CREATE TABLE drinks(nummer, drinkname PRIMARY KEY, preis, alkoholgehalt, menge)"
 
     c.execute(sql)
     conn.commit()
@@ -52,6 +58,10 @@ def check_password(username, password):
     else:
         return True
 
+def insert_drink(nummer, drinkname, preis, alkoholgehalt, menge):
+    a = "'"
+    c.execute('INSERT INTO drinks (nummer, drinkname, preis, alkoholgehalt, menge) VALUES (' + str(nummer) + ', ' + a + drinkname + a + ', ' + str(preis) + ', ' + str(alkoholgehalt) + ', ' + str(menge) + ')')
+    conn.commit()
 
 def insert_user(username, password):
     if check_if_user_exists(username) == True:
@@ -70,6 +80,45 @@ def show_table(table_name):
     print(sql)
     for row in c.execute(sql):
         print(row)
+
+def get_all_drinks():
+    sql = 'SELECT * FROM drinks'
+    c.execute(sql)
+    drinks = c.fetchall()
+    return drinks
+
+def get_all_numbers():
+    sql = 'SELECT nummer FROM drinks'
+    c.execute(sql)
+    numbers = c.fetchall()
+    return numbers
+
+def get_price(nummer):
+    sql=('SELECT preis FROM drinks WHERE nummer = ' + str(nummer))
+    c.execute(sql)
+    price = c.fetchone()
+    return int(price[0])
+
+def get_alcohol_content(nummer):
+    a = "'"
+    sql=('SELECT alkoholgehalt FROM drinks WHERE nummer = ' + str(nummer))
+    c.execute(sql)
+    alcohol_content = c.fetchone()
+    return float(alcohol_content[0])
+
+def get_volume(nummer):
+    a = "'"
+    sql=('SELECT menge FROM drinks WHERE nummer = ' + str(nummer))
+    c.execute(sql)
+    volume = c.fetchone()
+    return int(volume[0])
+
+def get_drinkname(nummer):
+    a = "'"
+    sql=('SELECT drinkname FROM drinks WHERE nummer = ' + str(nummer))
+    c.execute(sql)
+    drinkname = c.fetchone()
+    return str(drinkname[0])
 
 def get_balance(username):
     a = "'"
@@ -114,9 +163,27 @@ def delete_all_users():
 
 if __name__ == "__main__":
     
-    insert_user('testuser','testpass')
-    show_table('user')
-    show_table('bank')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """delete_table('drinks')
+    create_table('drinks')
+    insert_drink('1', 'Bier', '5', '4.8', '50')
+    insert_drink('2', 'Bier klein', '5', '4.8', '33')
+    insert_drink('3', 'Wein', '7', '12.5', '10')
+    insert_drink('4', 'Whisky', '15', '40.0', '2')
+    insert_drink('5', 'Wodka', '12', '37.5', '2')
+    insert_drink('6', 'cocktail', '10', '15.0', '4') --- Drinks debug und so, nicht löschen! ---""" 
+
     
 
 
